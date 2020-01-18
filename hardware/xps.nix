ffcefcff90c84b4fcb0583ee85ce5946ae6f04c0
Aaron Janse <ajanse@ajanse.me>
2020-01-18 14:46:53 -0800

    specify exact kernel derivation

--- hardware/xps.nix
+++ hardware/xps.nix
@@ -43,14 +43,32 @@ in
     naturalScrolling = true;
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_5_3;
-  boot.kernelPatches = [
-    {
-      name = "fix-display";
-      patch = pkgs.fetchpatch {
-        url = "https://bugs.freedesktop.org/attachment.cgi?id=144765";
-        sha256 = "sha256-Fc6V5UwZsU6K3ZhToQdbQdyxCFWd6kOxU6ACZKyaVZo=";
-      };
-    }
-  ];
+  boot.kernelPackages =
+    let
+      linux_pkg =
+        { stdenv, buildPackages, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
+        with stdenv.lib;
+        buildLinux (args // rec {
+          version = "5.3.16";
+
+          modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
+
+          kernelPatches = [
+            {
+              name = "fix-display";
+              patch = pkgs.fetchpatch {
+                url = "https://bugs.freedesktop.org/attachment.cgi?id=144765";
+                sha256 = "sha256-Fc6V5UwZsU6K3ZhToQdbQdyxCFWd6kOxU6ACZKyaVZo=";
+              };
+            }
+          ];
+
+          src = fetchurl {
+            url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+            sha256 = "19asdv08rzp33f0zxa2swsfnbhy4zwg06agj7sdnfy4wfkrfwx49";
+          };
+        } // (args.argsOverride or {}));
+        linux = pkgs.callPackage linux_pkg {};
+    in pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux);
+
 }
